ece844bf90e84788572c1a8b75e65f0e634a63f9
Prelang Builder <builder@prelang.com>
2016-12-05 12:41:17 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Harvest' to allow for create, update

--- app/admin/harvest.rb
+++ app/admin/harvest.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Harvest do
+  permit_params :harvest_date
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
