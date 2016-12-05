3b87c93f18ea5ecdd51f724dc2e5b180551d4ce2
Prelang Builder <builder@prelang.com>
2016-12-05 12:40:21 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Conversion' to allow for create, update

--- app/admin/conversion.rb
+++ app/admin/conversion.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Conversion do
+  permit_params :expression
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
