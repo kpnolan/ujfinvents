e7a09bde6b40cc7449d20df60ee39c7c8cdaa943
Prelang Builder <builder@prelang.com>
2016-12-05 12:40:34 +0000

    Generated scaffolding for model 'Metric' via Rails

---
+++ app/views/metrics/_metric.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! metric, :id, :value, :created_at, :updated_at
+json.url metric_url(metric, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :metrics
+
   resources :unit_measures
 
   resources :conversions
