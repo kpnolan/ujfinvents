f0947cda2715de7bd64cfb8acb5fadc65d84e4e7
Prelang Builder <builder@prelang.com>
2016-12-05 12:40:29 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'UnitMeasure' to allow for create, update

--- app/admin/unit_measure.rb
+++ app/admin/unit_measure.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register UnitMeasure do
+  permit_params :nominal_reference
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
