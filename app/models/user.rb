545c6bb21962f1e9f2d8ef50da7fbe2ea40ca5c2
Prelang Builder <builder@prelang.com>
2016-12-05 12:40:04 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
   #->Prelang (user_login/devise)
   belongs_to :event
+  belongs_to :measurement
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
 
